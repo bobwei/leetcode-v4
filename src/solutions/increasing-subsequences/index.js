bf693a75b18764f69b86ae4f09f68ddfe66907a6
bobwei <bob61016@gmail.com>
2017-02-07 14:16:14 +0800

    replace Map with cache object to have a better performance, pass

--- src/solutions/increasing-subsequences/index.js
+++ src/solutions/increasing-subsequences/index.js
@@ -1,11 +1,15 @@
-/* eslint-disable no-continue */
+/* eslint-disable no-continue, no-param-reassign */
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
-const findSubsequences = (nums, start = 0, output = new Map(), current = []) => {
+const findSubsequences = (nums, start = 0, output = [], current = [], cache = {}) => {
   if (current.length >= 2) {
-    output.set(current.toString(), [...current]);
+    const key = current.toString();
+    if (!cache[key]) {
+      cache[key] = true;
+      output.push([...current]);
+    }
   }
   for (let i = start; i < nums.length; i++) {
     /* ensure increasing */
@@ -13,10 +17,10 @@ const findSubsequences = (nums, start = 0, output = new Map(), current = []) => 
       continue;
     }
     current.push(nums[i]);
-    findSubsequences(nums, i + 1, output, current);
+    findSubsequences(nums, i + 1, output, current, cache);
     current.pop();
   }
-  return [...output.values()];
+  return output;
 };
 
 export default findSubsequences;
--- src/solutions/increasing-subsequences/index.spec.js
+++ src/solutions/increasing-subsequences/index.spec.js
@@ -1,6 +1,5 @@
+/* eslint-disable global-require */
 import findSubsequences from './index';
-import largeInput from './testCases/largeInput.json';
-import largeExpectedResult from './testCases/largeExpectedResult.json';
 
 it('findSubsequences [4, 6, 7, 7]', () => {
   const input = [4, 6, 7, 7];
@@ -19,11 +18,20 @@ it('findSubsequences [4, 3, 2, 1]', () => {
 });
 
 it('findSubsequences large case', () => {
+  const largeInput = require('./testCases/case1/largeInput.json');
+  const largeExpectedResult = require('./testCases/case1/largeExpectedResult.json');
   expect(findSubsequences(largeInput).sort())
     .toEqual(largeExpectedResult.sort());
 });
 
-it('findSubsequences large case [1,2,3,4,5,6,7,8,9,10,11]', () => {
-  const input = JSON.parse('[1,2,3,4,5,6,7,8,9,10,11]');
-  findSubsequences(input);
+it('findSubsequences large case [1,2,3,4,5,6,7,8,9,10,11,12]', () => {
+  const largeInput = require('./testCases/case2/largeInput.json');
+  const largeExpectedResult = require('./testCases/case2/largeExpectedResult.json');
+  expect(findSubsequences(largeInput).sort())
+    .toEqual(largeExpectedResult.sort());
+});
+
+it('findSubsequences large case [1,2,3,4,5,6,7,8,9,10,11,12] test performance', () => {
+  const largeInput = require('./testCases/case2/largeInput.json');
+  findSubsequences(largeInput);
 });
